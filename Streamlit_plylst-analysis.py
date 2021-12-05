687136f7cad9fe88e0475c59fcda553b938bdfe2
q-gtsan <52015079+q-gtsan@users.noreply.github.com>
2021-12-05 23:06:01 +0100

    update

--- Streamlit_plylst-analysis.py
+++ Streamlit_plylst-analysis.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-#packages needed
-#streamlit
-#pandas
-#plotly
-#
-
-#df = pd.read_csv("/Users/q.t./Documents/GitHub/Scrapping/wdr2-plylst (2).csv", sep=",")
 
 # Connecting to SQL-Database
 
@@ -17,6 +10,8 @@ df = pd.read_csv("plylst_wdr2.csv", sep=",")
 # df["Datum"] = pd.to_datetime(df["Datum"], format="%Y-%m-%d %H:%M")
 df['Datum'] = df['Datum'].map(lambda x: x.rstrip('Uhr'))
 df["Datum"] = pd.to_datetime(df["Datum"], format="%d.%m.%Y,%H.%M ")
+df["Interpret"] = df.Interpret.astype("category")
+df["Song"] = df.Song.astype("category")
 df["Jahr"] = df.Datum.dt.year
 df["Monat"] = df.Datum.dt.month
 df["Tag"] = df.Datum.dt.day
@@ -49,13 +44,21 @@ unique_day = sorted(df_select.Tag.unique())
 select_day = st.sidebar.multiselect("Tag", unique_day, unique_day)
 
 # Filtering data
-
+#Showing Data
 # df_select = df[(df.Monat.isin(select_month)) & (df.Tag.isin(select_day))]
+
 df_select = df[(df.Tag.isin(select_day))]
+st.header("Overall table of selected playlist")
+st.write("Data Dimension: "+str(df_select.shape[0]) + ' rows and 3 columns')
 st.write(df_select[["Datum", "Song", "Interpret"]])
 
+st.header("Most played Song")
 st.table(df_select["Song"].value_counts().head())
+st.header("Most played Artist")
 st.table(df_select["Interpret"].value_counts().head())
+st.header("Most played Artist and Song")
+df_group = df_select.groupby(["Interpret"]).Song.value_counts().sort_index(ascending=False).sort_values(ascending=False)
+st.table(df_group.head(30))
 
 # Descriptive Analysis
 
